f718c4a4a71d4d019d9cdf2c019ec6c1df6e32a4
fahim <ehsanulhoquefahim@gmail.com>
2023-02-27 00:26:35 +0600

    nada

---
+++ app/app.js
@@ -0,0 +1,8 @@
+const samllNav = document.querySelector('.side-nav');
+const btn = document.querySelector('.ham')
+
+btn.addEventListener('click', () => {
+    samllNav.classList.toggle('showcase')
+   
+       console.log('hello')
+  });
